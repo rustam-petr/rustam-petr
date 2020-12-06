235f48b8f171d8a8b1c9cfc7966d68bb05343f25
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2020-12-06 11:17:43 +0300

    Create osebe.php

---
+++ osebe.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "<body style='background-color:pink'>"?>
+    <h1> <?php echo 'Добрый день ' ?> </h1>
+    <?php echo 'Меня зовут Рустам и я хочу стать ВЕБ-разработчиком' ?>
+</body>
+</html>
