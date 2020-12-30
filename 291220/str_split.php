255732107317e76bb458f3fdccaeb0573baece9a
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2020-12-30 21:09:12 +0300

    exe file

--- 291220/str_split.php
+++ 291220/str_split.php
@@ -2,11 +2,14 @@
 $a = $_POST["q1"];
 $q = str_split($a);
  print_r($q);
+//  gettype($q);
 $number = ["ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"];
 echo "<br>";
 foreach ($q as $value) {
-   
-        echo $number[$value];
-    
+    if (is_numeric($value)) {
+        echo " ".$number[$value];
+       
+    } else {
+        echo " ".$value;
     }
-
+}
