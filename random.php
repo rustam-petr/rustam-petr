7d13162588a91f654aac612e0dc84ee2d6bb4356
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-01-04 23:41:22 +0300

    random

---
+++ random.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = 0;
+
+$b = 0;
+
+$arr = [];
+
+
+while ($a < $i = random_int(12, 20)) {
+
+    if ($i <> 15) {
+
+        array_push($arr, $i);
+        $b = $b + 1;
+    } else {
+        break;
+    }
+}
+
+print_r($arr);
+echo "<br>";
+echo "Выполнено операций цикла " . $b;
