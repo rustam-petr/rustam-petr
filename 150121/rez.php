f4e81b3f2931bb0f9a15f3bf62cf0026cccb3ac5
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-01-20 17:44:51 +0300

    change

--- 150121/rez.php
+++ 150121/rez.php
@@ -1,13 +1,16 @@
 <?php
-$date = file("rez.txt");
+$date = explode('.', file_get_contents('rez.txt'));
 
-$answer = $_POST["answer"];
+// $answer = $_POST["answer"];
 
-$date[$answer]=($date[$answer]+1)."\n";
+$date[$_POST["answer"]]++;
+print_r($date);
 
-$saveDate=$date[0].$date[1].$date[2].$date[3];
 
-$fb=fopen("rez.txt", "w+");
-fwrite($fb,$saveDate);
-fclose($fb);
-echo $saveDate;
+// $saveDate=$date[0].$date[1].$date[2].$date[3];
+file_put_contents('rez.txt', implode('.', $date));
+
+// $fb=fopen("rez.txt", "w+");
+// fwrite($fb,$saveDate);
+// fclose($fb);
+// echo $saveDate;
--- 190121/explode.php
+++ 190121/explode.php
@@ -1,5 +1,5 @@
 <?php
 $arr = explode(':', file_get_contents('file1.txt'));
-$arr[2]++;
+$arr[3]++;
 print_r($arr);
 file_put_contents('file1.txt', implode(':', $arr));
