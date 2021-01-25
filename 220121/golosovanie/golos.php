221c4069fc9bb6700437f0199e57e191b3bfdcd1
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-01-25 12:48:21 +0300

    change

--- 220121/golosovanie/golos.php
+++ 220121/golosovanie/golos.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <?php
     include "config.php";
     $data = file($fileName);
- 
+
     if (isset($_POST['answer'])) {
         $currentAnswer = $_POST['answer'];
         $buff = explode($separate, $data[$currentAnswer + 1]);
@@ -20,27 +21,30 @@
         $data[$currentAnswer + 1] = $buff[0] . $separate . $buff[1];
         file_put_contents($fileName, implode("", $data));
     }
+
     $question = $data[0];
     unset($data[0]);
-
 
     $answers = [];
     foreach ($data as $value) {
         $answers[] = explode($separate, $value);
     }
-  print_r($answers);
     ?>
 
     <form action="?" method="POST">
-        <?= $question ?>
-        <br>
-        <?php
-        foreach ($answers as $key => $value) {
-            $count = $value[1] * 1;
-            echo "<input type='radio' value='$key' name='answer'> $value[0] ($count) <br>\n";
-        }
-        ?>
-        <input type="submit" value="ok">
+        <div class="wer">
+            <div class="q">
+                <?= $question ?>
+            </div>
+            <br>
+            <?php
+            foreach ($answers as $key => $value) {
+                $count = $value[1] * 1;
+                echo "<input type='radio' value='$key' name='answer'> $value[0] ($count) <br>\n";
+            }
+            ?>
+            <input type="submit" value="ok">
+        </div>>
     </form>
 </body>
 
