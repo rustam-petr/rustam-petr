8f3040b1aed55f88d293a15642e1d1d060344b1c
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-01-28 21:16:37 +0300

    Update gost.php

--- 260121/gost.php
+++ 260121/gost.php
@@ -1,3 +1,41 @@
 <?php
-$answer="меня зовут Рустам";
-fwrite("gost.txt","w+");
+if (!empty($_POST)) {
+    $row = "\n<----->\n" .
+        $_POST["name"] . "\n" .
+        $_POST["email"] . "\n" .
+        $_POST["text"];
+    file_put_contents("guest.txt", $row, FILE_APPEND);
+    header("Location: ?");
+    die();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $data = file_get_contents("guest.txt");
+    $records = explode("<----->", $data);
+    echo "<table border='1'>";
+    foreach ($records as $record) {
+        $row = explode("\n", trim($record));
+        // print_r($row);
+        echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
+    }
+    echo "</table>";
+    ?>
+    <form action="?" method="POST">
+        <input type="text" name="name">
+        <input type="text" name="email">
+        <input type="text" name="text">
+        <input type="submit" value="ok">
+    </form>
+</body>
+
+</html>
