9d8cfbae9ccee69f1691ea865efe2799c1a7d3e9
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-02-02 15:50:01 +0300

    change

--- 260121/gost.php
+++ 260121/gost.php
@@ -7,27 +7,37 @@ include "gost1.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Косячок</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <?php
     $data = file_get_contents("dan.txt");
     $records = explode("<----->", $data);
+    ?>
+    <div class="otzyv">
+        <?php
     echo "<table border='1'>";
     foreach ($records as $record) {
-        $time=date("H:i:s");
         $row = explode("\n", trim($record));
-        echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$time</td></tr>";
+        echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td></tr>";
     }
     echo "</table>";
     ?>
+    </div>
+    <br>
+    <div class="menu">
     <form action="?" method="POST">
-        <input type="text" name="name">
-        <input type="text" name="email">
-        <input type="text" name="text">
-        <input type="submit" value="ok">
+        Введите Ваше имя. <br>
+        <input type="text" name="name"> <br>
+        Введите Ваш адрес электронной почты.<br>
+        <input type="text" name="email"><br>
+        Введите информацию.<br>
+        <textarea rows="20" cols="30" name="text"></textarea><br><br>
+        <input type="submit" value="Оставить отзыв">
     </form>
+    </div>
 </body>
 
 </html>
--- 260121/gost1.php
+++ 260121/gost1.php
@@ -1,9 +1,10 @@
 <?php
-if (!empty($_POST)) {
+if ((!empty($_POST)) && ($_POST["name"] != "") && ($_POST["text"] != "")) {
     $row = "\n<----->\n" .
         $_POST["name"] . "\n" .
         $_POST["email"] . "\n" .
-        $_POST["text"];
+        $_POST["text"]."\n".
+        date("d F Y H:i:s");
     file_put_contents("dan.txt", $row, FILE_APPEND);
     header("Location: ?");
     die();
