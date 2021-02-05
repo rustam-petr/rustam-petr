3a8449b5ab29297d4e1e3f292f1b394fb64b025e
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-02-05 17:32:01 +0300

    change

--- newlesson/privet.php
+++ newlesson/privet.php
@@ -1,7 +1,9 @@
 <?php
 //$str = $_POST["name"];
 //$pat = "/" . $_POST["slo"] . "/i";
-echo "Количество совпадений - " . $rez = preg_match_all("/" . $_POST["slo"] . "/iu", $_POST["name"]);
 //echo "Количество совпадений - " . $rez;
+
+echo "Количество совпадений - " . $rez = preg_match_all("/\b" . $_POST["slo"] . "\b/iu", $_POST["name"]);
+
 
 
--- newlesson/zamena.php
+++ newlesson/zamena.php
@@ -1,6 +1,7 @@
 <?php
 //$str = $_POST["text"];
 //$pat =  $_POST["zam"] ;
-echo $regexp = "/" . $_POST["qwe"] . "/iu";
-//echo $rez = preg_replace("/" . $_POST["qwe"] . "/iu", $_POST["zam"], $_POST["text"]);
+//echo $regexp = "/\b" . $_POST["qwe"] . "\b/iu";
 //echo $rez;
+
+echo $rez = preg_replace("!\b" . $_POST["qwe"] . "\b!iu", $_POST["zam"], $_POST["text"]);
