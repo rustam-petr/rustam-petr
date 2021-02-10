27a54c46da6134ac67121078ae6ee2f3d40b4a87
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-02-10 22:06:08 +0300

    change

--- 090221/domzad/gost.php
+++ 090221/domzad/gost.php
@@ -1,31 +1,18 @@
 <?php
-$str=$_POST["fio"];
-$regexp="/[a-zа-яё][ ]{1,}[a-zа-яё]/iu";
-$q=preg_match_all($regexp,$str);
-$str1=$_POST["pochta"];
-$regexp1="/[a-zа-яё\d\+_]@[a-zа-яё\d]{4,}\.[a-zа-яё]{2,}/iu";
-$q1=preg_match_all($regexp1,$str1);
-$str2=$_POST["telefon"];
-$regexp2="/^(\+375)((29)?|(33)?|(44)?)(\d{7})$/iu";
-$q2=preg_match_all($regexp2,$str2);
-$str3=$_POST["adress"];
-$regexp3="/^(vk)\.(id)\d{8}$/iu";
-$q3=preg_match_all($regexp3,$str3);
+$str = preg_match_all("/\w[ ]\w/iu", $_POST["fio"]);
+$str1 = preg_match("/[\w\d\+_]@[\w\d]{4,}\.[\w]{2,}/iu", $_POST["pochta"]);
+$str2 = preg_match("/(80|\+375)(25|29|33|44)(\d{7})/iu", $_POST["telefon"]);
+$str3 = preg_match("/^(vk)\.(id)\d{8}$/iu", $_POST["adress"]);
 
-$qwe=$str.$str1.$str2.$str3; // формируем строку из введенных значений
-$qwe1=preg_replace("/[ ]{1,}/iu","",$qwe); // убираем все пробелы
+$string = preg_replace("/[ ]{1,}/iu", "", ($_POST["pochta"] . $_POST["telefon"]));
+$read = preg_replace("/[ \n]{1,}/iu", "", file_get_contents("gost.txt"));
 
+$pos = strripos($read, $string);
 
-$read=file_get_contents("gost.txt");
-$arr=preg_replace("/[ \n]{1,}/iu","",$read);
-
-$pos=strripos($arr,$qwe1);
-
-if ($pos===false) {
-
-    if ((($q == $q1) == $q2) == $q3) {
+if ($pos === false) {
+    if ($str == 1 && $str1 == 1 && $str2 == 1 && $str3 == 1) {
         echo "Вы успешно прошли регистрацию";
-        file_put_contents("gost.txt", ($str . "\n" . $str1 . "\n" . $str2 . "\n" . $str3 . "\n"),FILE_APPEND);
+        file_put_contents("gost.txt", ($_POST["fio"] . "\n" . $_POST["pochta"] . "\n" . $_POST["telefon"] . "\n" . $_POST["adress"] . "\n"), FILE_APPEND);
     } else {
         echo "Введите правильные данные";
     }
