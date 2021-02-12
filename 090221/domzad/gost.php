1f8c70c7a6d29bdc583d832cd3e7baf12714d17c
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-02-12 11:59:56 +0300

    +

--- 090221/domzad/gost.php
+++ 090221/domzad/gost.php
@@ -1,6 +1,6 @@
 <?php
-$str = preg_match_all("/\w[ ]\w/iu", $_POST["fio"]);
-$str1 = preg_match("/[\w\d\+_]@[\w\d]{4,}\.[\w]{2,}/iu", $_POST["pochta"]);
+$str = preg_match_all("/\w[ ]\w/iu", trim($_POST["fio"]));
+$str1 = preg_match("/[\w\d\+_]@[\w\d]{4,}\.[\w]{2,}/iu", trim($_POST["pochta"]));
 $telefon=preg_replace("/[-()]/","",$_POST["telefon"]);
 $str2 = preg_match("/(80|\+375)(25|29|33|44)(\d{7})/iu", $telefon);
 $str3 = preg_match("/^(vk)\.(id)\d{8}$/iu", $_POST["adress"]);
@@ -15,7 +15,7 @@ $pos1 = strripos($read, $string1);
 if ($pos === false && $pos1 === false) {
     if ($str == 1 && $str1 == 1 && $str2 == 1 && $str3 == 1) {
         echo "Вы успешно прошли регистрацию";
-        file_put_contents("gost.txt", ($_POST["fio"] . "\n" . $_POST["pochta"] . "\n" . $telefon . "\n" . $_POST["adress"] . "\n"), FILE_APPEND);
+        file_put_contents("gost.txt",( trim($_POST["fio"]) . "\n" . trim($_POST["pochta"]) . "\n" . $telefon . "\n" . $_POST["adress"] . "\n"), FILE_APPEND);
     } else {
         echo "Введите правильные данные";
     }
