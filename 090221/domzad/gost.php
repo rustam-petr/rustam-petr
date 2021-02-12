30ba4363e126b86d00b06f99eb1c176ce14bc306
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-02-12 11:31:00 +0300

    change domzad

--- 090221/domzad/gost.php
+++ 090221/domzad/gost.php
@@ -1,18 +1,21 @@
 <?php
 $str = preg_match_all("/\w[ ]\w/iu", $_POST["fio"]);
 $str1 = preg_match("/[\w\d\+_]@[\w\d]{4,}\.[\w]{2,}/iu", $_POST["pochta"]);
-$str2 = preg_match("/(80|\+375)(25|29|33|44)(\d{7})/iu", $_POST["telefon"]);
+$telefon=preg_replace("/[-()]/","",$_POST["telefon"]);
+$str2 = preg_match("/(80|\+375)(25|29|33|44)(\d{7})/iu", $telefon);
 $str3 = preg_match("/^(vk)\.(id)\d{8}$/iu", $_POST["adress"]);
 
-$string = preg_replace("/[ ]{1,}/iu", "", ($_POST["pochta"] . $_POST["telefon"]));
+$string = preg_replace("/[ ]{1,}/iu", "", $_POST["pochta"]);
+$string1 = preg_replace("/[ ]{1,}/iu", "", $_POST["telefon"]);
 $read = preg_replace("/[ \n]{1,}/iu", "", file_get_contents("gost.txt"));
 
 $pos = strripos($read, $string);
+$pos1 = strripos($read, $string1);
 
-if ($pos === false) {
+if ($pos === false && $pos1 === false) {
     if ($str == 1 && $str1 == 1 && $str2 == 1 && $str3 == 1) {
         echo "Вы успешно прошли регистрацию";
-        file_put_contents("gost.txt", ($_POST["fio"] . "\n" . $_POST["pochta"] . "\n" . $_POST["telefon"] . "\n" . $_POST["adress"] . "\n"), FILE_APPEND);
+        file_put_contents("gost.txt", ($_POST["fio"] . "\n" . $_POST["pochta"] . "\n" . $telefon . "\n" . $_POST["adress"] . "\n"), FILE_APPEND);
     } else {
         echo "Введите правильные данные";
     }
