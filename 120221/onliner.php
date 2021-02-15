bfb94bc35eef34d79966fc590c79167aa5d57668
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-02-15 21:48:44 +0300

    change

--- 120221/onliner.php
+++
@@ -1,8 +0,0 @@
-<?php
-$a = $_POST["url"];
-$str = file_get_contents($a);
-$regexp = "/<div class=\"b-main-page-news-2__main-news-text\">\n?([ ]+)?<p>[\w.,\s-]+<\/p>\n?([ ]+)?<\/div>/iu";
-$q = preg_match_all($regexp, $str, $matches);
-foreach ($matches[0] as $match) {
-    echo $match."<br>";
-}
