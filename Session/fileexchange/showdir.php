be8d6f2d17103aea412bbb0118aedc89539579e1
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-02-19 18:15:42 +0300

    +

--- Session/fileexchange/showdir.php
+++ Session/fileexchange/showdir.php
@@ -6,15 +6,19 @@ sort($dir);
 foreach ($dir as $value) {
     if (preg_match("/.*\.dll/iu",$value)){
         $qwe=preg_replace("/(.*)\.[a-z]+/iu", "<img src='image-dll.png'>"."$1",$value);
+        echo $qwe."<br>";
     }
     if (preg_match("/.*\.docx/iu",$value)){
         $qwe=preg_replace("/(.*)\.[a-z]+/iu", "<img src='image-doc.png'>"."$1",$value);
+        echo $qwe."<br>";
     }
     if (preg_match("/.*\.pdf/iu",$value)){
         $qwe=preg_replace("/(.*)\.[a-z]+/iu", "<img src='image-pdf.png'>"."$1",$value);
+        echo $qwe."<br>";
     }
-//    if (preg_match("/.*\.\w{3,}[^dll][^pdf][^docx]/iu",$value)){
-//        $qwe=preg_replace("/(.*)\.[a-z]+/iu", "<img src='image-other.png'>"."$1",$value);
-//    }
-    echo "<a href='files\\$value'>$qwe</a><br>";
+    if (preg_match("/.*\.[^(dll)]+|[^(pdf)]+|[^(docx)]+/iu",$value)){
+        $qwe=preg_replace("/(.*)\.[a-z]+/iu", "<img src='image-other.png'>"."$1",$value);
+        echo $qwe."<br>";
+    }
+//    echo "<a href='files\\$value'>$qwe</a><br>";
 }
