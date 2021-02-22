fb7d1520e8b4a091310a6c57d29e5588a1c0bcc2
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-02-22 23:25:42 +0300

    ++

--- Projekt GuestBook/add.php
+++ Projekt GuestBook/add.php
@@ -1,6 +1,16 @@
 <?php
-//print_r($_POST);
-$data=json_decode(file_get_contents("data.json"));
-$data[]=["msg"=>$_POST["msg"], "name"=>$_POST["name"]];
-file_put_contents("data.json",json_encode($data));
-header("Location:main.php");
+include "smile.php";
+
+if (!qwe($_POST["msg"])) {
+    $data = json_decode(file_get_contents("data.json"));
+    $data[] = ["msg" => $_POST["msg"], "name" => $_POST["name"], "date/time" => date("d F Y H:i:s")];
+    file_put_contents("data.json", json_encode($data));
+} else {
+    preg_match_all("/.*?(дурак|идиот|редиска|козел).*?/iu", $_POST["msg"], $matches);
+    foreach ($matches[1] as $match) {
+        file_put_contents("file.txt", $match . "\n", FILE_APPEND);
+
+    }
+}
+
+header("Location: main.php");
--- Projekt GuestBook/main.php
+++ Projekt GuestBook/main.php
@@ -6,23 +6,26 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div class="pop">
 <?php
 include "smile.php";
 $data = json_decode(file_get_contents("data.json"), true);
 foreach ($data as $row) {
-    if (!qwe($row["msg"])) {
-        echo smile($row["msg"]) . " " . $row["name"] . "<br>";
-    }
+        echo "<p>".date("d F Y (l)")."<br>". smile($row["msg"]) . "<br>". $row["name"] . "<br>"."</p>";
 }
 ?>
+</div>
+<div class="tut">
 <form action="add.php" method="post">
-    <textarea name="msg" cols="30" rows="30"></textarea><br>
+    <textarea name="msg" cols="50" rows="5"></textarea><br>
     <input type="text" name="name">
-    <input type="submit" value="ok">
+    <input type="submit" value="✅">
 
 </form>
+</div>
 </body>
 </html>
 
