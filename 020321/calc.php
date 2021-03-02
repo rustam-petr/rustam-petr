8ee9670c06467c5df5ec08d616911d467239c929
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-03-02 22:19:20 +0300

    add

---
+++ 020321/calc.php
@@ -0,0 +1,30 @@
+<?php
+preg_match("/(\d)(\+|-|\*|\/)(\d)/iu", $_POST["calc"], $matches);
+$a = $matches[1];
+$b = $matches[3];
+$operation = $matches[2];
+switch ($operation) {
+    case "+":
+        $result = $a + $b;
+        break;
+    case "-":
+        $result = $a - $b;
+        break;
+    case "*":
+        $result = $a * $b;
+        break;
+    case "/":
+        $result = $a / $b;
+        break;
+    default:
+        $result = "Unknown operation";
+}
+echo $result;
+echo match ($operation) {
+    "+" => $a + $b,
+    "-" => $a - $b,
+    "*" => $a * $b,
+    "/" => $a / $b,
+    default => "Unknown operation"
+
+};
