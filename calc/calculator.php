c9ba2232abe86ae8b18459b29d5c7fb8d88d48d1
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-03-11 09:50:58 +0300

    ++

---
+++ calc/calculator.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-1">
+                <img src="css/img/logo.png" class="img-fluid logo" alt="калькулятор">
+            </div>
+            <div class="col">
+                <p class="fw-bolder "><span class="fw-bolder__head">Calculator</span><span class="fw-bolder__dot">.by</span><br>Онлайн-калькуляторы</p>
+            </div>
+            <div class="col-4">
+                <p class="text-end fst-italic"><span class="head__size">РАЗРАБОТКА </span>
+                    <span class="fw-bolder__dot">ОНЛАЙН-КАЛЬКУЛЯТОРОВ </span>
+                    <span class="head__color">ЛЮБОЙ СЛОЖНОСТИ</span>
+                </p>
+            </div>
+            <div class="col line">
+                <a href="#" class="link-success">Подробнее...</a><br><br>
+                <button type="button" class="btn  active btn-success" data-bs-toggle="button" autocomplete="off" aria-pressed="true">
+                    Заказать калькулятор
+                </button>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="line__botton"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-color">Главная</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Математические калькуляторы</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Финансовые калькуляторы</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Калькуляторы веса и калорий</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Калькуляторы для строительства и ремонта</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Таможенные калькуляторы</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Прочие кальуляторы</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Информация</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Карта сайта</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Разработка калькулятора для сайта</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Пример наших работ</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Конвертеры</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Отзывы клиентов</a>
+                    <a href="#" class="list-group-item list-group-item-action list-group-item-dark">Помощь проекту</a>
+
+                </div>
+            </div>
+            <div class="col-7">
+                <div class="row row-cols-1 row-cols-md-2 g-4">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title"><img src="css/img/1.png" alt="...">Математический калькулятор</h5>
+                                <p class="card-text">
+                                    <a href="#" class="card-link none" tabindex="-1" aria-disabled="true">Стандартный калькулятор</a> <br>
+                                    <a href="#" class="card-link none" tabindex="-1" aria-disabled="true">Калькулятор дробей</a> <br>
+                                    <a href="#" class="card-link none" tabindex="-1" aria-disabled="true">Калькулятор площади</a> <br>
+                                </p>
+                            </div>
+                        </div>
+                        <a href="#" class="link-success">Подробнее>></a>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title"><img src="css/img/2.png" alt="...">Калькулятор веса и калорий</h5>
+                                <p class="card-text">
+                                    <a href="#" class="card-link none" tabindex="-1" aria-disabled="true">Калькулятор идеального веса</a> <br>
+                                    <a href="#" class="card-link none" tabindex="-1" aria-disabled="true">Калькулятор индекса массы тела</a> <br>
+                                    <a href="#" class="card-link none" tabindex="-1" aria-disabled="true">Калькулятор калорийности продуктов</a> <br>
+                                </p>
+                            </div>
+                        </div>
+                        <a href="#" class="link-success">Подробнее>></a>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title"><img src="css/img/3.png" alt="...">Таможенные калькуляторы</h5>
+                                <p class="card-text">
+                                    <a href="#" class="card-link none" tabindex="-1" aria-disabled="true">Легковые автомобили</a> <br>
+                                    <a href="#" class="card-link none" tabindex="-1" aria-disabled="true">Грузовые автомобили</a> <br>
+                                    <a href="#" class="card-link none" tabindex="-1" aria-disabled="true">Прицепы</a> <br>
+                                </p>
+                            </div>
+                        </div>
+                        <a href="#" class="link-success">Подробнее>></a>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title"><img src="css/img/4.png" alt="...">Конверторы</h5>
+                                <p class="card-text">
+                                    <a href="#" class="card-link none" tabindex="-1" aria-disabled="true">Конвертер единиц длины</a> <br>
+                                    <a href="#" class="card-link none" tabindex="-1" aria-disabled="true">Конвертер единиц площади</a> <br>
+                                    <a href="#" class="card-link none" tabindex="-1" aria-disabled="true">Конвертер единиц массы</a> <br>
+                                </p>
+                            </div>
+                        </div>
+                        <a href="#" class="link-success">Подробнее>></a>
+                    </div>
+                </div>
+                <br>
+                <br>
+                СЮДА МОЖНО ВСТАВИТЬ СВОЙ КАЛЬКУЛЯТОР
+            </div>
+        </div>
+        <div class="col">
+
+        </div>
+    </div>
+
+    <footer class="footer">
+        <p class="text-center"><span class="fw-bolder__dot">Онлайн-калькуляторы</span><br> © 2021 Calculator.by Все права защищены</p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+
+</html>
