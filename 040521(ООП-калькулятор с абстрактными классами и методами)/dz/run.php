02d60ff33d447899a5afb4cfde380200957179b9
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-05-05 12:42:30 +0300

    Update run.php

--- 040521(ООП-калькулятор с абстрактными классами и методами)/dz/run.php
+++ 040521(ООП-калькулятор с абстрактными классами и методами)/dz/run.php
@@ -2,23 +2,24 @@
 include 'autoload.php';
 
 
-$calc = new NewCalc1(30, 60);
+$calc = new NewCalc1( 180,90);
 
-//echo $calc->sum();
-//
-//echo "<br>";
-//
-//echo $calc->mult();
-//
-//echo "<br>";
-//
-//echo $calc->div();
-//
-//echo "<br>";
-//
-//echo $calc->diff();
-//
-//echo "<br>";
+echo $calc->sum();
+
+echo "<br>";
+
+echo $calc->mult();
+
+echo "<br>";
+
+echo $calc->div();
+
+echo "<br>";
+
+echo $calc->diff();
+
+echo "<br>";
 
 echo $calc->sin();
+echo "<br>";
 echo $calc->sin1();
