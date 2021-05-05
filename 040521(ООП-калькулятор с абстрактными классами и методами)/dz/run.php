2ba5a72e4b0cf9d2a7c28d7bf9cb260104255121
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-05-05 12:18:50 +0300

    add calc sin and cos

---
+++ 040521(ООП-калькулятор с абстрактными классами и методами)/dz/run.php
@@ -0,0 +1,24 @@
+<?php
+include 'autoload.php';
+
+
+$calc = new NewCalc1(30, 60);
+
+//echo $calc->sum();
+//
+//echo "<br>";
+//
+//echo $calc->mult();
+//
+//echo "<br>";
+//
+//echo $calc->div();
+//
+//echo "<br>";
+//
+//echo $calc->diff();
+//
+//echo "<br>";
+
+echo $calc->sin();
+echo $calc->sin1();
