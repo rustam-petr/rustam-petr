9c2b0a78b39a0dd671c591f1a8cf62d4fce29af2
rustam-petr <74614737+rustam-petr@users.noreply.github.com>
2021-05-24 21:32:37 +0300

    ret

--- 210521dz/run.php
+++ 210521dz/run.php
@@ -1,1 +1,11 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Action\Calc;
+
+$calc=new Calc(5,7);
+
+echo $calc->sum()."<br>";
+echo $calc->mult()."<br>";
+echo $calc->diff()."<br>";
+echo $calc->div()."<br>";
